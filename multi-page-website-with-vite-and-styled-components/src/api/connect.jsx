fc54d8c40b67746e61a14d10e6311d14126ffd98
ali baltaci <alibaltaci13@gmail.com>
2023-08-28 20:41:53 +0300

    removed mongodb

--- multi-page-website-with-vite-and-styled-components/src/api/connect.jsx
+++ multi-page-website-with-vite-and-styled-components/src/api/connect.jsx
@@ -1,18 +1,18 @@
-import { MongoClient } from "mongodb"
+// import { MongoClient } from "mongodb"
 
-export async function connectDatabase(){
+// export async function connectDatabase(){
 
-    const mongoClient = new MongoClient('mongodb+srv://alibaltaci13:g4MtCxMHwIgohwcA@cluster0.vzhdfac.mongodb.net/react_multi_page_website?retryWrites=true&w=majority')
+//     const mongoClient = new MongoClient('mongodb+srv://alibaltaci13:g4MtCxMHwIgohwcA@cluster0.vzhdfac.mongodb.net/react_multi_page_website?retryWrites=true&w=majority')
 
-    const data = await mongoClient.db().collection('services').find({}).toArray()
+//     const data = await mongoClient.db().collection('services').find({}).toArray()
    
-    // const client = await MongoClient.connect('mongodb+srv://alibaltaci13:g4MtCxMHwIgohwcA@cluster0.vzhdfac.mongodb.net/react_multi_page_website?retryWrites=true&w=majority',
-    //     {
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology: true
-    //     }
-    // )
+//     // const client = await MongoClient.connect('mongodb+srv://alibaltaci13:g4MtCxMHwIgohwcA@cluster0.vzhdfac.mongodb.net/react_multi_page_website?retryWrites=true&w=majority',
+//     //     {
+//     //         useNewUrlParser: true,
+//     //         useUnifiedTopology: true
+//     //     }
+//     // )
 
-    return data;
+//     return data;
     
-}
+// }
