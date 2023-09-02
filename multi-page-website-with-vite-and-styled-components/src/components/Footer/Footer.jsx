5fd8804b6668cca477974a6ee7154e3d6834a761
ali baltaci <alibaltaci13@gmail.com>
2023-09-03 00:17:54 +0300

    footer

--- multi-page-website-with-vite-and-styled-components/src/components/Footer/Footer.jsx
+++ multi-page-website-with-vite-and-styled-components/src/components/Footer/Footer.jsx
@@ -1,20 +1,60 @@
 import Container from "../Common/Container/Container"
 import Button from "../UI/Button/Button"
 import Typography from "../UI/Typography/Typography"
-import { StyledFooterWrapper } from "./StyledFooter"
+import Form from "../Common/Form/Form"
+import { StyledFooter, StyledFooterBottom, StyledFooterTop, StyledFooterWrapper } from "./StyledFooter"
 
 function Footer() {
+
+  const date = new Date().getFullYear()
   return (
     <StyledFooterWrapper>
-      <Container grid="2"  >
-        <Container direction="column" gap="1rem" marginTop="2rem" >
-          <Typography elementType="h3" text="Ready Go Get Started?" />
-          <Typography elementType="h3" text="Talk To Us Today" />
-        </Container>
-        <Container direction="column" gap="1rem" >
-          <Button route="/" text="Get Started" />  
+      <StyledFooterTop>
+        <Container grid="2"  >
+          <Container direction="column" gap="1rem" marginTop="2rem" >
+            <Typography elementType="h3" text="Ready Go Get Started?" />
+            <Typography elementType="h3" text="Talk To Us Today" />
           </Container>
-      </Container>  
+          <Container direction="column" gap="1rem" >
+            <Button route="/" text="Get Started" />  
+            </Container>
+        </Container>
+      </StyledFooterTop>
+
+      <StyledFooter>
+        <Container grid="4">
+          <div>
+            <Typography elementType="h3" text="Sisyphus Soft" color="white" marginBottom="2.4rem" />
+            <Typography text="Lorem ipsum dolor sit amet consectetur adipisicing elit."  color="white" />
+          </div>
+          <div>
+            <Typography elementType="h3" text="Subscribe to get important updates"  color="white" marginBottom="2.4rem" />
+            <Form />
+          </div>
+          {/* social icons */}
+          <div>
+            <Typography elementType="h3" text="Follow Us" color="white" marginBottom="2.4rem" />
+          </div>
+          <div>
+            <Typography elementType="h3" text="Call Us" color="white" marginBottom="2.4rem" />
+            <Typography elementType="h3" text="0 555 000 00 00" color="white" />
+          </div>
+        </Container>
+      </StyledFooter>
+      <hr />
+      <StyledFooterBottom>
+        <Container direction="row">
+          <Container direction="row" >
+            <Typography text={`${date} Sisyphus Soft. All Rights Reserved.`} color="white" />
+          </Container>
+          <Container>
+            <Typography text="PRIVACY POLICY" color="white" />
+            <Typography text="TERMS & CONDITIONS" color="white" />
+          </Container>
+        </Container>
+      </StyledFooterBottom> 
+
+
     </StyledFooterWrapper>
   )
 }
--- multi-page-website-with-vite-and-styled-components/src/components/Footer/StyledFooter.jsx
+++ multi-page-website-with-vite-and-styled-components/src/components/Footer/StyledFooter.jsx
@@ -2,21 +2,33 @@ import { styled } from "styled-components"
 
 export const StyledFooterWrapper = styled.section`
 
+
+
+`
+
+export const StyledFooterTop = styled.div`
     max-width: 60vw;
     margin: auto;
     padding: 2rem;
     background-color: ${({theme}) => theme.colors.bg};
     border-radius: 1rem;
     box-shadow: ${({theme}) => theme.colors.shadowSupport};
+    transform: translateY(50%);
 
     div:last-child{
         justify-self: end;
         align-self: center;
     }
-
 `
 
 export const StyledFooter = styled.footer`
 
+    padding: 14rem 0 9rem 0;
+    background-color: ${({theme}) => theme.colors.footer_bg};
 
+`
+
+export const StyledFooterBottom = styled.div`
+    background-color: ${({theme}) => theme.colors.footer_bg};
+    padding: 2rem 0 2rem;
 `
