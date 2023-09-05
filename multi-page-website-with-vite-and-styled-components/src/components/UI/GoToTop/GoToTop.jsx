fabbeaef28fc452fa960a836af801ffca68a7fba
ali baltaci <alibaltaci13@gmail.com>
2023-09-06 00:00:00 +0300

    finished GoToTop

--- multi-page-website-with-vite-and-styled-components/src/components/UI/GoToTop/GoToTop.jsx
+++ multi-page-website-with-vite-and-styled-components/src/components/UI/GoToTop/GoToTop.jsx
@@ -1,15 +1,45 @@
 import { StyledGoToTop } from "./StyledGoToTop";
 import ArrowUp from "../../Common/Icons/ArrowUp";
+import { useEffect, useState } from "react";
 
 export default function GoToTop() {
+
+  const [isVisible, setIsVisible] = useState(false)
 
   const goToBtn = () => {
     window.scrollTo( { top:0, left: 0, behavior: "smooth" } )
   }
 
+  const listenToScroll = () => {
+    let heightToHidden = 250;
+
+    // for all browsers  ( body || html )
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+    if( winScroll > heightToHidden ){
+      setIsVisible(true)
+    }else{
+      setIsVisible(false)
+    }
+    
+  }
+
+  useEffect( () => {
+
+    window.addEventListener( "scroll", listenToScroll )
+
+    return () => window.removeEventListener("scroll", listenToScroll)
+
+  }, [] )
+
+
   return (
-    <StyledGoToTop onClick={ goToBtn }>
-        <ArrowUp />
-    </StyledGoToTop>
+      <>
+        { isVisible  &&
+          <StyledGoToTop onClick={ goToBtn }>
+              <ArrowUp />
+          </StyledGoToTop>
+        }
+      </>
   )
 }
