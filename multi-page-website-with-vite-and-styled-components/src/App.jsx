10f07a7e6936cb4df7d5815af0f6380aeadbe288
ali baltaci <alibaltaci13@gmail.com>
2023-09-08 14:41:07 +0300

    404 page optimized

--- multi-page-website-with-vite-and-styled-components/src/App.jsx
+++ multi-page-website-with-vite-and-styled-components/src/App.jsx
@@ -1,27 +1,9 @@
-import './App.css'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import GoToTop from './components/UI/GoToTop/GoToTop'
-import RoutesComponent from './RoutesComponent'
+import RoutesComponent from '../src/components/RoutesComponent'
 
 function App() {
 
   return (
-    <>
-      <Header />
-      {/* <Routes>
-          {
-          navbar.map( (data) =>( 
-              <Route key={data.path} path={data.path} element={ <RouteReturner page={data.page} />} />
-            ))
-          }
-      </Routes> */}
-
       <RoutesComponent/>
-      <GoToTop />
-      <Footer />
-
-    </>
   )
 }
 
---
+++ multi-page-website-with-vite-and-styled-components/src/components/RoutesComponent.jsx
@@ -0,0 +1,80 @@
+import { useRoutes } from "react-router-dom"
+import About from "../pages/About"
+import Contact from "../pages/Contact/Contact"
+import Home from "../pages/Home"
+import Page404 from "../pages/Page404/Page404"
+import Services from "../pages/Services/Services"
+import Laypout from "./Layout/Laypout"
+
+// const routes = [
+
+//     {
+//         page:"home",
+//         path:"/",
+//         element: <Home />
+//     },
+//     {
+//         page:"about",
+//         path:"/about",
+//         element: <About />
+//     },
+//     {
+//         page:"services",
+//         path:"/services",
+//         element: <Services />
+//     },
+//     {
+//         page:"contact",
+//         path:"/contact",
+//         element: <Contact />
+//     },
+//     {
+//         page:"page404",
+//         path:"/*",
+//         element: <Page404 />
+//     },
+    
+// ]
+const routes = [
+
+    {
+        page:"home",
+        path:"/",
+        element: <Laypout />,
+        children:[
+            {
+                page:"index",
+                index:true,
+                element: <Home />
+            },
+            {
+                page:"about",
+                path:"about",
+                element: <About />
+            },
+            {
+                page:"services",
+                path:"services",
+                element: <Services />
+            },
+            {
+                page:"contact",
+                path:"contact",
+                element: <Contact />
+            },
+        ]
+    },
+
+    {
+        page:"page404",
+        path:"/*",
+        element: <Page404 />
+    },
+    
+]
+
+export default function RoutesComponent() {
+  return (
+    useRoutes(routes)
+  )
+}
