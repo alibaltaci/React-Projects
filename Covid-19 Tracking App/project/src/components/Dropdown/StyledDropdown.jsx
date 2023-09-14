de98b4f5f056a629ad96e6a52f124aab3dc5b33f
ali baltaci <alibaltaci13@gmail.com>
2023-09-14 22:38:24 +0300

    hover

--- Covid-19 Tracking App/project/src/components/Dropdown/StyledDropdown.jsx
+++ Covid-19 Tracking App/project/src/components/Dropdown/StyledDropdown.jsx
@@ -7,21 +7,26 @@ export const StyledDropdown = styled.select`
     border: none;
     outline: none;
     border-bottom: 1px solid ${({theme}) => theme.colors.pastel_green};
+    cursor: pointer;
 
     color: ${({theme}) => theme.colors.pastel_green};
 
     font-weight: 600;
     font-size: 1.2rem;
-
-
+    
     /* appearance: none;
     -webkit-appearance: none;
     -moz-appearance: none; */
+
+    &:hover{
+        background-color:${({theme}) => theme.colors.deep_fuchsia } ;
+        border-radius: 10px;
+        border: none;
+    }
 
     option{
         background-color: ${({theme}) => theme.colors.aubergine};
         font-size: 1.1rem;
     }
-
 
 `
