14a2210acec917698bf9429c1c640cbdd4faf8c8
ali baltaci <alibaltaci13@gmail.com>
2023-10-09 11:45:10 +0300

    checkbox and input

--- Chat App/project/src/components/Input/Input.jsx
+++ Chat App/project/src/components/Input/Input.jsx
@@ -1,6 +1,6 @@
 import { FlexContainer } from "../FlexContainer/FlexContainer"
 import  Typography  from "../Typography/Typography"
-import { StyledTextArea } from "./StyledInput"
+import { StyledInput, StyledTextArea } from "./StyledInput"
 
 const InputTitle = ({isRequired, title, titleVariant}) => {
 
@@ -34,6 +34,7 @@ export const Input = ( {title, isRequired, titleVariant = "paragraph_min", ...pr
   const isTitleArray = Array.isArray(title)
 
   // from input tag
+  // text-area
   if(props.type === "text-area"){
     return(
       <FlexContainer type="column" >
@@ -68,5 +69,83 @@ export const Input = ( {title, isRequired, titleVariant = "paragraph_min", ...pr
       </FlexContainer>
     )
   }
+
+  // checkbox
+  if(props.type ===  "checkbox"){
+    return(
+      <FlexContainer type="row" >
+        <StyledInput {...props} />
+        {
+          title && !isTitleArray && (
+            <InputTitle 
+              isRequired={isRequired}
+              title={title}
+              titleVariant={titleVariant}
+            />
+          )
+        }
+        {
+          title && isTitleArray && (
+            <FlexContainer type="column" >
+              {title.map( t => (
+                <InputTitle 
+                  key={t}
+                  isRequired={isRequired}
+                  title={t}
+                  titleVariant={titleVariant}
+                />
+              ))}
+            </FlexContainer>
+          )
+        }
+      </FlexContainer>
+    )
+  }
+
+  // input
+  return(
+    <FlexContainer type="column" >
+      {
+        title && !isTitleArray && (
+          <InputTitle
+            title={title}
+            isRequired={isRequired}
+            titleVariant={titleVariant}
+          />
+        )
+      }
+      {
+        title && isTitleArray && (
+          <FlexContainer type="column">
+            {
+              title.map( t => (
+                <InputTitle 
+                  key={t}
+                  title={title}
+                  isRequired={isRequired}
+                  titleVariant={titleVariant}
+                />
+              ))
+            }
+          </FlexContainer>
+        )
+      }
+
+      <StyledInput {...props} />
+
+      {
+        !!props.errorMessage && (
+          <Typography 
+            text={props.errorMessage}
+            elementType="paragraph_min"
+            color="red_default"
+            marginTrim
+          />
+        )
+      }
+
+    </FlexContainer>
+  )
+
 
 }
