e9bdb76761aa66814028c9e183e883b1d28050a5
ali baltaci <alibaltaci13@gmail.com>
2023-10-10 16:13:17 +0300

    Background Image Container

---
+++ Chat App/project/src/components/BackgroundImageContainer/BackgroundImageContainer.jsx
@@ -0,0 +1,21 @@
+import { Image } from "../Image/Image"
+import { BackgroundOpacityWrapper, ContentContainer, StyledBackgroundImageContainer } from "./StyledBackgroudImageContainer"
+
+export const BackgroundImageContainer = ({children, bgOpacity, aspectRatio ="1.65", imageUrl, loading, alt, ...props}) => {
+    
+    return(
+        <StyledBackgroundImageContainer aspectRatio={aspectRatio} {...props} >
+            <BackgroundOpacityWrapper opacity={bgOpacity} borderRadius={props.borderRadius} >
+                <Image 
+                    src={imageUrl}
+                    loading={loading}
+                    alt={alt}
+                />
+            </BackgroundOpacityWrapper>
+            <ContentContainer>
+                {children}
+            </ContentContainer>
+        </StyledBackgroundImageContainer>
+
+    )
+}
