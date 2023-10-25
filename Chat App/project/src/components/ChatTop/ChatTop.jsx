8fad5d63da7fc0d9cd08660c6e4eea2ca4234247
ali baltaci <alibaltaci13@gmail.com>
2023-10-25 20:48:39 +0300

    icon style

--- Chat App/project/src/components/ChatTop/ChatTop.jsx
+++ Chat App/project/src/components/ChatTop/ChatTop.jsx
@@ -1,17 +1,19 @@
 import { FlexContainer, ProfileImage, ReactIconReturner, Typography } from "../UI"
 import { AiOutlineMenu } from "react-icons/ai";
 import { useGlobalContext } from "../../contexts/GlobalContext";
+import { useHoverColor } from "../../hooks";
 
 export const ChatTop = () => {
 
   const icons = ["cam", "person", "options"]
 
   const { handleVisible, isResponsive } = useGlobalContext()
+  const { clr, handleColorOver, handleColorOut } = useHoverColor("white", "plum")
 
     return(
         <FlexContainer type="row" gap="1rem" backgroundColor="black_default" height="6.3rem" radiusTwo="s" align="center" justify="space-between" horizonalPadding="2rem" >
         <FlexContainer type="row" align="center" gap="1rem" >
-          { isResponsive && <AiOutlineMenu color="white" size="2rem" onClick={handleVisible} /> }
+          { isResponsive && <AiOutlineMenu color={clr} size="2rem" onClick={handleVisible} cursor="pointer" onMouseOver={handleColorOver} onMouseOut={handleColorOut} /> }
           <FlexContainer type="row" align="center" >
             <ProfileImage src="./images/balloon-profile.svg" alt="profile" loading="lazy" width="3rem" height="3rem" bgColor="white" marginRight="1rem" />
             <Typography elementType="span" text="Alex" color="white_default" variant="parahraph_xl" />
--- Chat App/project/src/components/Navbar/Navbar.jsx
+++ Chat App/project/src/components/Navbar/Navbar.jsx
@@ -3,10 +3,13 @@ import BalloonChatIcon from "../UI/Icons/BalloonChatIcon";
 import { UserCard } from "..";
 import {RxCross2} from "react-icons/rx"
 import { useGlobalContext } from "../../contexts/GlobalContext";
+import { useHoverColor } from "../../hooks";
 
 export const Navbar = () => {
 
   const {isResponsive, handleVisible} = useGlobalContext()
+
+  const { clr, handleColorOver, handleColorOut } = useHoverColor("white", "plum")
 
   return (
     <FlexContainer type="row" align="center" height="6.5rem" backgroundColor="black_default" radiusOne="s" horizonalPadding="0.5rem" >        
@@ -14,7 +17,7 @@ export const Navbar = () => {
         <UserCard owner />
         <FlexContainer type="row" align="center" gap="1rem" >
           <Button text="Logout" size="s" />
-          { isResponsive && <RxCross2 color="white" size="2rem" onClick={handleVisible} />}
+          { isResponsive && <RxCross2 color={clr} size="2rem" onClick={handleVisible} cursor="pointer" onMouseOver={handleColorOver} onMouseOut={handleColorOut} />}
         </FlexContainer>
     </FlexContainer>
   )
