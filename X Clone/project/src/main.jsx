aed9b9e1233a73b788fed23a47703dcfe4e63d90
ali baltaci <alibaltaci13@gmail.com>
2023-11-14 17:10:45 +0300

    routes

--- X Clone/project/src/main.jsx
+++ X Clone/project/src/main.jsx
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client'
-
+import { RouterProvider } from "react-router-dom"
+import routes from './routes'
 
 // {/* <React.StrictMode>  */}  iki kere render edilmesinin sebebi bu moddur. 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
+  <RouterProvider router={routes} />
 )
--- X Clone/project/vite.config.js
+++ X Clone/project/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // jsconfig
+  resolve:{
+    alias:{
+      // eslint -> env --> node: true 
+        '~': path.resolve(__dirname, 'src'),
+    }
+  }
 })
