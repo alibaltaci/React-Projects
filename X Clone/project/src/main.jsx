7d16bcd5362581d2b4397ba89c1241928e1b4996
ali baltaci <alibaltaci13@gmail.com>
2023-11-14 16:04:07 +0300

    project: x clone

---
+++ X Clone/project/src/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+
+
+// {/* <React.StrictMode>  */}  iki kere render edilmesinin sebebi bu moddur. 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+)
