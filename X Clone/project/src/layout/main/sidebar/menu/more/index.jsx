ba25f3dab51f421b283317e65ef2562b86d51b62
ali baltaci <alibaltaci13@gmail.com>
2023-11-21 19:11:16 +0300

    transform

--- X Clone/project/src/layout/main/sidebar/menu/more/index.jsx
+++ X Clone/project/src/layout/main/sidebar/menu/more/index.jsx
@@ -3,6 +3,7 @@ import { Disclosure } from '@headlessui/react'
 import { Link } from "react-router-dom";
 import { IconReturner } from "~/components/icons";
 
+
 const data = {
     disclosureData : [
         {
@@ -10,7 +11,8 @@ const data = {
             contents: [
                 {
                     content:"Analytics",
-                    icon: "graph"
+                    icon: "graph",
+                    link: "https://analytics.twitter.com/about"
                 }
             ]
         },
@@ -19,7 +21,8 @@ const data = {
             contents: [
                 {
                     content:"Ads",
-                    icon: "ad"
+                    icon: "ad",
+                    link: "https://ads.twitter.com/onboarding/18ce55n7h9v/welcome?ref=gl-tw-tw-twitter-ads-rweb"
                 }
             ]
         },
@@ -32,7 +35,8 @@ const data = {
                 },
                 {
                     content:"Help Centery",
-                    icon: "help"
+                    icon: "help",
+                    link: "https://help.twitter.com/"
                 },
                 {
                     content:"Display",
@@ -48,6 +52,7 @@ const data = {
 }
 
 export default function More({text, icon}) {
+  
   return (
     <Popover className="relative group"> 
         <Popover.Button  className="py-1 block  outline-none" >
@@ -74,21 +79,36 @@ export default function More({text, icon}) {
                 data.disclosureData.map( (disclosure, index) => {
                     const {title, contents} = disclosure
                     return <Disclosure key={index} >
-                        <Disclosure.Button className="h-[52px] w-full flex items-center px-4 font-bold justify-between hover:bg-[#eff3f41a] transition-colors">
-                            { title }
-                            <IconReturner icon="arrow" />
-                        </Disclosure.Button>
-                        {
-                            contents.map( (el, index) => {
-                                const { content, icon } = el
-                                return <Disclosure.Panel key={index} className="hover:bg-[#eff3f41a] transition-colors flex">
-                                    <Link to="/" className="flex items-center px-3 h-11 gap-3 font-medium text-[15px]">
-                                        <IconReturner icon={ icon } />
-                                        { content }
-                                    </Link>
-                                </Disclosure.Panel>
-                            } )
-                        }
+                        {({ open }) => (
+                            <>
+                                <Disclosure.Button className="h-[52px] w-full flex items-center px-4 font-bold justify-between hover:bg-[#eff3f41a] transition-colors">
+                                    { title }
+                                    <div className={`transform duration-100 ${ open && '-rotate-180  text-[#1d9bf0] '}`}>
+                                        <IconReturner icon="arrow" />
+                                    </div>
+                                </Disclosure.Button>
+                                {
+                                    contents.map( (el, index) => {
+                                        const { content, icon, link } = el
+                                        return <Disclosure.Panel key={index} className="hover:bg-[#eff3f41a] transition-colors flex">
+                                            {
+                                                link ? 
+                                                    <Link to={link} target="_blank" className="flex items-center px-3 h-11 gap-3 font-medium text-[15px]">
+                                                        <IconReturner icon={ icon } />
+                                                        { content }
+                                                    </Link>
+                                                    : 
+                                                    <div className="flex items-center px-3 h-11 gap-3 font-medium text-[15px]">
+                                                        <IconReturner icon={ icon } />
+                                                        { content }
+                                                    </div>
+                                            }
+                                        </Disclosure.Panel>
+                                    } )
+                                }
+                            </>
+                        )}
+
                     </Disclosure>
                 })
             }
