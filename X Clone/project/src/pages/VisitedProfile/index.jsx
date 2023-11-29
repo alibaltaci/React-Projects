5b6b3223f288d67d1b0f55acad9c1f2810103b81
ali baltaci <alibaltaci13@gmail.com>
2023-11-29 17:17:25 +0300

    profile page helmet

--- X Clone/project/src/pages/VisitedProfile/index.jsx
+++ X Clone/project/src/pages/VisitedProfile/index.jsx
@@ -1,12 +1,21 @@
+import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom"
+import { useAccount } from "~/store/auth/hooks";
 
 export default function VisitedProfile() {
 
     const { slug } =useParams();
 
+    const {fullName} = useAccount()
+
   return (
+    <>
+      <Helmet >
+        <title> {fullName} (@{slug}) </title>
+      </Helmet>    
     <div>
         Visited Profile - { slug }
     </div>
+    </>
   )
 }
