4e6b77778ee239b31979fe8aba51f058468e2fc2
ali baltaci <alibaltaci13@gmail.com>
2023-12-18 01:15:00 +0300

    premium

---
+++ X Clone/project/src/components/premium/index.jsx
@@ -0,0 +1,8 @@
+
+export default function Premium() {
+  return (
+    <section className="bg-[#16181c] mb-4 rounded-2xl border border-[#16181c] py-4 px-3 ">
+        premium
+    </section>
+  )
+}
