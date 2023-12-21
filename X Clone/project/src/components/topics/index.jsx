e7f56908cd161366dc3043dd5117bb095067206a
ali baltaci <alibaltaci13@gmail.com>
2023-12-21 13:21:52 +0300

    progress

--- X Clone/project/src/components/topics/index.jsx
+++ X Clone/project/src/components/topics/index.jsx
@@ -1,6 +1,8 @@
 
 export default function Topics() {
   return (
-    <div>Topics</div>
+    <div className="bg-[#16181c] mb-4 rounded-2xl border border-[#16181c]">
+      <h5 className="h-12 flex items-center">Trends for you</h5>
+    </div>
   )
 }
--- X Clone/project/src/layout/main/rightbar/index.jsx
+++ X Clone/project/src/layout/main/rightbar/index.jsx
@@ -1,11 +1,13 @@
 import Premium from "~/components/premium";
 import Search from "./search";
+import Topics from "~/components/topics";
 
 export default function Rightbar() {
   return (
     <aside className="w-[350px] mr-5" >
       <Search />
       <Premium />
+      <Topics />
     </aside>
   )
 }
