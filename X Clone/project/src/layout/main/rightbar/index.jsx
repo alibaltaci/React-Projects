f3c5e371d02ffe2a98c5b82b5af5d32efa8b54a2
ali baltaci <alibaltaci13@gmail.com>
2024-02-08 19:35:32 +0300

    go

--- X Clone/project/src/layout/main/rightbar/index.jsx
+++ X Clone/project/src/layout/main/rightbar/index.jsx
@@ -3,6 +3,7 @@ import Search from "./search";
 import Topics from "~/components/topics";
 
 export default function Rightbar() {
+  
   return (
     <aside className="w-[350px] mr-5" >
       <Search />
