d80951a3015661782586e1acd19c50d74a968fa3
ali baltaci <alibaltaci13@gmail.com>
2024-02-14 23:39:50 +0300

    .

--- X Clone/project/src/layout/main/rightbar/index.jsx
+++ X Clone/project/src/layout/main/rightbar/index.jsx
@@ -5,10 +5,12 @@ import Topics from "~/components/topics";
 export default function Rightbar() {
   
   return (
+
     <aside className="w-[350px] mr-5" >
       <Search />
       <Premium />
       <Topics />
     </aside>
+  
   )
 }
